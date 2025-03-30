9dc824e3bb91338b7cd8bd47ee7532ead3c0f72c
Peter Potrok <peter.potrok@outlook.sk>
2025-03-30 06:11:15 +0200

    Add files via upload

---
+++ CDspStandards.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+* \file      CDspStandards.h
+* \version   2025.03.18.01
+* \author    Peter Potrok
+* \copyright Copyright (c) 1994 - 2025
+*            MIT License (see License.txt file)
+* \brief     C Dsp Standard definitions
+* \details
+*
+* \see       https://en.wikipedia.org/wiki/Digital_signal_processing
+*            https://github.com/cpredef/predef
+******************************************************************************/
+
+#ifndef CDSPSTANDARDS_H
+#define CDSPSTANDARDS_H		20250318L
+
+
+/* RFU - Reserved for Future Use */
+
+
+#endif /* CDSPSTANDARDS_H */
